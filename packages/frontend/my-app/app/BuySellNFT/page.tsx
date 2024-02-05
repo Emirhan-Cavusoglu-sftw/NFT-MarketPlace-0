3925a83432a003d5049b834bf38a200ffc9c56d9
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.xom>
2024-02-06 01:32:41 +0300

    sheesh fix

--- packages/frontend/my-app/app/BuySellNFT/page.tsx
+++ packages/frontend/my-app/app/BuySellNFT/page.tsx
@@ -1,148 +1,117 @@
 "use client";
-import React from 'react'
+import React from "react";
 import { uploadFileToIPFS, uploadJSONToIPFS } from "../utils/pinata";
-import { useState } from 'react';
+import { useState } from "react";
 import { useContractRead, useContractWrite, useAccount } from "wagmi";
-import { writeContract } from 'wagmi/actions';
+import { writeContract } from "wagmi/actions";
 import nftMarketPlaceABI from "../abis/nftMarketPlaceABI.json";
-import { parseEther } from 'viem'
+import { parseEther } from "viem";
 const Page = () => {
-    const contractAddress = "0xe92C8d48e1dad7Cb83719396cc5d5BA36FBFF70E";
-    const [formParams, updateFormParams] = useState({ name: '', description: '', price: ''});
-    const [fileURL, setFileURL] = useState(null);
-    const [message, updateMessage] = useState('');
-    
-    // WAGMI HOOKS
-    const {
-        data: dataPay,
-        isLoading: loadingPay,
-        isSuccess: successPay,
-        write: createToken,
-      } = useContractWrite({
+  const contractAddress = "0x424778313C58D929B8D948d28e4D70dBB742b135";
+  const [formParams, updateFormParams] = useState({
+    name: "",
+    description: "",
+    price: "",
+  });
+  const [fileURL, setFileURL] = useState(null);
+  const [message, updateMessage] = useState("");
+
+  // WAGMI HOOKS
+  const {
+    data: dataPay,
+    isLoading: loadingPay,
+    isSuccess: successPay,
+    write: createToken,
+  } = useContractWrite({
+    address: contractAddress,
+    abi: nftMarketPlaceABI,
+    functionName: "createToken",
+  });
+
+  const {data:listingPrice}  = useContractRead({
+    address: contractAddress,
+    abi: nftMarketPlaceABI,
+    functionName: "getListPrice",
+  });
+
+  const listedPrice = listingPrice?.toString();
+console.log(listedPrice);
+
+  async function OnChangeFile(e: any) {
+    var file = e.target.files[0];
+    //check for file extension
+    try {
+      //upload the file to IPFS
+
+      const response = await uploadFileToIPFS(file);
+      if (response.success === true) {
+        console.log("Uploaded image to Pinata: ", response.pinataURL);
+        setFileURL(response.pinataURL);
+      }
+    } catch (e) {
+      console.log("Error during file upload", e);
+    }
+  }
+  async function uploadMetadataToIPFS() {
+    const { name, description, price } = formParams;
+    //Make sure that none of the fields are empty
+    if (!name || !description || !price || !fileURL) {
+      updateMessage("Please fill all the fields!");
+      return -1;
+    }
+
+    const nftJSON = {
+      name,
+      description,
+      price,
+      image: fileURL,
+    };
+
+    try {
+      //upload the metadata JSON to IPFS
+      const response = await uploadJSONToIPFS(nftJSON);
+      if (response.success === true) {
+        console.log("Uploaded JSON to Pinata: ", response);
+        return response.pinataURL;
+      }
+    } catch (e) {
+      console.log("error uploading JSON metadata:", e);
+    }
+  }
+
+  async function createNFT(e: any) {
+    e.preventDefault();
+
+    try {
+      const metadataURL = await uploadMetadataToIPFS();
+      if (metadataURL === -1) return;
+
+      updateMessage(
+        "Uploading NFT(takes 5 mins).. please dont click anything!"
+      );
+
+      const price = parseEther(formParams.price);
+
+      console.log("listedPrice", listedPrice);
+
+      await writeContract({
         address: contractAddress,
         abi: nftMarketPlaceABI,
         functionName: "createToken",
+        args: [metadataURL, price],
+        value: listedPrice,
       });
-    
-      const {data :listingPrice}= useContractRead({
-        address: contractAddress,
-        abi: nftMarketPlaceABI,
-        functionName: "getListPrice",
-      });
-    
-      
-    const listedPrice= listingPrice.toString();
-    
-    
-    
-    
-    
-    
-    
-    async function OnChangeFile(e: any) {
-        var file = e.target.files[0];
-        //check for file extension
-        try {
-            //upload the file to IPFS
-            
-            const response = await uploadFileToIPFS(file);
-            if(response.success === true) {
-                
-                console.log("Uploaded image to Pinata: ", response.pinataURL)
-                setFileURL(response.pinataURL);
-            }
-        }
-        catch(e) {
-            console.log("Error during file upload", e);
-        }
+      // await transaction.wait()
+
+      alert("Successfully listed your NFT!");
+
+      window.location.replace("/");
+    } catch (e) {
+      alert("Upload error" + e);
     }
-    async function uploadMetadataToIPFS() {
-        const {name, description, price} = formParams;
-        //Make sure that none of the fields are empty
-        if( !name || !description || !price || !fileURL)
-        {
-            updateMessage("Please fill all the fields!")
-            return -1;
-        }
+  }
 
-        const nftJSON = {
-            name, description, price, image: fileURL
-        }
-
-        try {
-            //upload the metadata JSON to IPFS
-            const response = await uploadJSONToIPFS(nftJSON);
-            if(response.success === true){
-                console.log("Uploaded JSON to Pinata: ", response)
-                return response.pinataURL;
-            }
-        }
-        catch(e) {
-            console.log("error uploading JSON metadata:", e)
-        }
-    }
-    
-
-
-
-    async function createNFT(e:any) {
-        e.preventDefault();
-
-        //Upload data to IPFS
-        try {
-            const metadataURL = await uploadMetadataToIPFS();
-            if(metadataURL === -1)
-                return;
-            //After adding your Hardhat network to your metamask, this code will get providers and signers
-            // const provider = new ethers.providers.Web3Provider(window.ethereum);
-            // const signer = provider.getSigner();
-            // disableButton();
-            updateMessage("Uploading NFT(takes 5 mins).. please dont click anything!")
-
-            //Pull the deployed contract instance
-            // let contract = new ethers.Contract(Marketplace.address, Marketplace.abi, signer)
-
-            //massage the params to be sent to the create NFT request
-            const price = parseEther(formParams.price)
-            // const listedPrice = listingPrice.toString()
-            console.log("listedPrice", listedPrice)
-
-            //actually create the NFT
-            //  createToken({args:[metadataURL, price]})
-
-            await writeContract({
-                address: contractAddress,
-                abi: nftMarketPlaceABI,
-                functionName: "createToken",
-                args: [metadataURL, price],
-                value: listedPrice
-            })
-            // await transaction.wait()
-
-            alert("Successfully listed your NFT!");
-            
-            window.location.replace("/")
-        }
-        catch(e) {
-            alert( "Upload error"+e )
-        }
-    }
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-    return (
+  return (
     <div className="flex flex-col place-items-center mt-10" id="nftForm">
       <form className="bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4">
         <h3 className="text-center font-bold text-purple-500 mb-8">
@@ -160,8 +129,9 @@ const Page = () => {
             id="name"
             type="text"
             placeholder="Axie#4563"
-            onChange={e => updateFormParams({...formParams, name: e.target.value})}
-            
+            onChange={(e) =>
+              updateFormParams({ ...formParams, name: e.target.value })
+            }
           ></input>
         </div>
         <div className="mb-6">
@@ -178,7 +148,9 @@ const Page = () => {
             id="description"
             type="text"
             placeholder="Axie Infinity Collection"
-            onChange={e => updateFormParams({...formParams, description: e.target.value})}
+            onChange={(e) =>
+              updateFormParams({ ...formParams, description: e.target.value })
+            }
           ></textarea>
         </div>
         <div className="mb-6">
@@ -193,7 +165,9 @@ const Page = () => {
             type="number"
             placeholder="Min 0.01 ETH"
             step="0.01"
-            onChange={e => updateFormParams({...formParams, price: e.target.value})}
+            onChange={(e) =>
+              updateFormParams({ ...formParams, price: e.target.value })
+            }
           ></input>
         </div>
         <div>
@@ -212,15 +186,11 @@ const Page = () => {
           id="list-button"
           onClick={createNFT}
         >
-          Create NFT 
+          Create NFT
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default page
-
-
-
-
+export default Page;
