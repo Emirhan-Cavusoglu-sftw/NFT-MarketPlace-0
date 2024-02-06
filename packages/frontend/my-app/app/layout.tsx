1054688216544f31e9e655e8894b767abba7e208
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-02-06 16:14:04 +0300

    bg changed

--- packages/frontend/my-app/app/layout.tsx
+++ packages/frontend/my-app/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>
+        <div className="background">
           <ConnectProvider>
             <Header />
             {children}
