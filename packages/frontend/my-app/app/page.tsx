557870b9a502104c9f503817a7ba0b469c1cdd90
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.xom>
2024-02-06 14:09:38 +0300

    format

--- packages/frontend/my-app/app/page.tsx
+++ packages/frontend/my-app/app/page.tsx
@@ -12,38 +12,6 @@ export default function Home() {
   const [data, updateData] = useState([]);
   const [dataFetched, updateFetched] = useState(false);
   const contractAddress = "0xbB6EB8CfA4790Aeb1AA6258c5A03DBD4f3Ac2386";
-  const sampleData = [
-    {
-      name: "NFT#1",
-      description: "Alchemy's First NFT",
-      website: "http://axieinfinity.io",
-      image:
-        "https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-      price: "0.03ETH",
-      currentlySelling: "True",
-      address: "0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-      name: "NFT#2",
-      description: "Alchemy's Second NFT",
-      website: "http://axieinfinity.io",
-      image:
-        "https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
-      price: "0.03ETH",
-      currentlySelling: "True",
-      address: "0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-      name: "NFT#3",
-      description: "Alchemy's Third NFT",
-      website: "http://axieinfinity.io",
-      image:
-        "https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-      price: "0.03ETH",
-      currentlySelling: "True",
-      address: "0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
-  ];
 
   async function getNFTData() {
     let transaction = await readContract({
