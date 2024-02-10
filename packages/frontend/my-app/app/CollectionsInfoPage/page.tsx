4b5145f34c84caad6805a5152c9f3b6a5c24a22b
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-02-10 16:04:12 +0300

    collectionsInfo page added

---
+++ packages/frontend/my-app/app/CollectionsInfoPage/page.tsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const CollectionsInfoPage = () => {
+  return (
+    <div>
+      <h1>CollectionsInfoPage</h1>
+    </div>
+  );
+};
+
+export default CollectionsInfoPage;
