cc7542282bb27d5b79cbdd3e279037e10cb1620a
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-02-12 16:23:46 +0300

    address changed

--- packages/frontend/my-app/app/utils/constants.js
+++ packages/frontend/my-app/app/utils/constants.js
@@ -1,2 +1,2 @@
 export const marketPlaceAddress = "0xbB6EB8CfA4790Aeb1AA6258c5A03DBD4f3Ac2386";
-export const collectionFactoryAddress = "0x7b1d96AadFD510B24D46f3371e9b2dFA1963BB11";
+export const collectionFactoryAddress = "0x7802AFEe32608016842558529df4191c306b7a6e";
