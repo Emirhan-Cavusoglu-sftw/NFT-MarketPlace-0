16dde4ffac2a6aabedfb849f046f9ca91ea4c88d
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-02-15 19:39:12 +0300

    contract abis changed

--- packages/frontend/my-app/app/utils/constants.js
+++ packages/frontend/my-app/app/utils/constants.js
@@ -1,2 +1,2 @@
 export const marketPlaceAddress = "0xbB6EB8CfA4790Aeb1AA6258c5A03DBD4f3Ac2386";
-export const collectionFactoryAddress = "0x6c1a3E8840af081968Be1c739cA414579FA176B4";
+export const collectionFactoryAddress = "0x9Ba211B4Cf649ACe5853C5AeAf7F9798A734C61B";
